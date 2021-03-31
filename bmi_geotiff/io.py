98de8a8d6fbe380c72c7feb64ea7a45215361756
Mark Piper <mark.piper@colorado.edu>
2021-03-31 15:51:15 -0600

    Write library and tests

--- bmi_geotiff/io.py
+++ bmi_geotiff/io.py
@@ -1,7 +1,40 @@
 """Access GeoTIFF files."""
+import xarray as xr
+
+
 class GeoTiff:
 
     """Access data and metadata in a GeoTIFF file."""
 
-    def __init__(self):
-        pass
+    def __init__(self, filename=None):
+        """Make a GeoTiff object.
+
+        Parameters
+        ----------
+        filename : str, optional
+            Path to the file to open.
+        """
+        self._da = None
+        self._filename = None
+
+        if filename is not None:
+            self.open(filename)
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def da(self):
+        return self._da
+
+    def open(self, filename):
+        """Load a GeoTIFF file into an xarray DataArray.
+
+        Parameters
+        ----------
+        filename : str
+            Path to the file to open.
+        """
+        self._filename = filename
+        self._da = xr.open_rasterio(self._filename)
--- tests/test_io.py
+++ tests/test_io.py
@@ -1,7 +1,46 @@
 """Test the bmi_geotiff.io module"""
+from pathlib import Path
+
+import pytest
+from rasterio.errors import RasterioIOError
+from xarray import DataArray
+
 from bmi_geotiff import GeoTiff
 
+TEST_FILE = "RGB.byte.tif"
 
-def test_instantiate():
+
+def test_instantiate_without_filename():
     g = GeoTiff()
     assert isinstance(g, GeoTiff)
+    assert g.filename is None
+    assert g.da is None
+
+
+def test_bad_filename(tmpdir, shared_datadir):
+    with pytest.raises(RasterioIOError):
+        GeoTiff("foo.tif")
+
+
+def test_with_filename(shared_datadir):
+    f = Path(shared_datadir) / TEST_FILE
+    g = GeoTiff(f)
+    assert isinstance(g, GeoTiff)
+    assert g.filename == f
+    assert isinstance(g.da, DataArray)
+
+
+def test_with_filename_as_keyword(shared_datadir):
+    f = Path(shared_datadir) / TEST_FILE
+    g = GeoTiff(filename=f)
+    assert isinstance(g, GeoTiff)
+    assert g.filename == f
+    assert isinstance(g.da, DataArray)
+
+
+def test_open(shared_datadir):
+    g = GeoTiff()
+    f = Path(shared_datadir) / TEST_FILE
+    g.open(f)
+    assert g.filename == f
+    assert isinstance(g.da, DataArray)
