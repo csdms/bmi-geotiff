87e1b1a58707ac1db3f86a5d2fd64b4549ee0df6
Mark Piper <mark.piper@colorado.edu>
2021-04-06 11:35:59 -0600

    Make script and notebook examples of GeoTiff class

---
+++ examples/readme_example.py
@@ -0,0 +1,19 @@
+"""An example of using the GeoTiff data component.
+
+The example is adapted from http://xarray.pydata.org/en/stable/examples/visualization_gallery.html#imshow()-and-rasterio-map-projections,
+and it uses the sample GeoTIFF image "RGB.byte.tif" from the rasterio project.
+"""
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+from bmi_geotiff import GeoTiff
+
+
+tif_file = "RGB.byte.tif"
+
+g = GeoTiff(tif_file)
+print(g.da)
+
+crs = ccrs.UTM('18N')
+ax = plt.subplot(projection=crs)
+g.da.plot.imshow(ax=ax, rgb='band', transform=crs)
+plt.show()
