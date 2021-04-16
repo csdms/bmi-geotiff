1551ece0babaa36b0c1743fe7f273991fdf7c361
Mark Piper <mark.piper@colorado.edu>
2021-04-16 13:48:32 -0600

    Add pan band example

---
+++ examples/example-pan.py
@@ -0,0 +1,19 @@
+"""An example of using the GeoTiff data component.
+
+The data used are band 1 of Landsat 7 ETM scene LE71030642021102ASA00.
+"""
+import matplotlib.pyplot as plt
+from bmi_geotiff import GeoTiff
+
+
+url = "https://csdms.colorado.edu/data/LE07_L1GT_103064_20210412_20210412_02_RT_B1.TIF"
+
+g = GeoTiff()
+g.open(url)
+print(g.da)
+
+band = g.da.squeeze("band")
+print(band)
+
+band.plot.imshow()
+plt.show()
