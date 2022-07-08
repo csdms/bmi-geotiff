dc3237dfdccaa986d8263b9e1b764670d09fa86c
Mark Piper <mark.piper@colorado.edu>
2022-07-08 15:23:06 -0600

    Fix typo

--- bmi_geotiff/__init__.py
+++ bmi_geotiff/__init__.py
@@ -1,5 +1,5 @@
+from ._version import __version__
 from .bmi import BmiGeoTiff
 from .io import GeoTiff
-from ._version import __version__
 
-__all__ = ["GeoTiff", "BmiGeoTiff", , "__version__"]
+__all__ = ["GeoTiff", "BmiGeoTiff", "__version__"]
