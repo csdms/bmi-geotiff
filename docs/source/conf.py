ca52a6fd779f164c9a63634c2b2ce1963dcc51b6
Mark Piper <mark.piper@colorado.edu>
2024-11-12 14:33:20 -0700

    Stop black and isort from fighting over conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,9 @@
-# Configuration file for the Sphinx documentation builder.
-#
+""" conf.py
+    Configuration file for the Sphinx documentation builder.
+
+   isort:skip_file
+"""
+
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -9,6 +13,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import datetime
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
